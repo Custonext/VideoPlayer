26a8ac52ee47aaf42e0301d5a553f531eabbf047
Bart van der Meeren <bartvandermeeren@gmail.com>
2022-12-29 13:51:35 +0100

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import CryptoList from "./components/CryptoList";
+import "./styles.css";
+import React from "react";
 
-function App() {
+export default function App(props) {
+  console.log(props.divElement.dataset.formId)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <CryptoList />
   );
 }
-
-export default App;
